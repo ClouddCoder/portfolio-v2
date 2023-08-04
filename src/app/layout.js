8bd960743801303502e90ae9547f3af7f17ac7e8
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-08-03 21:58:36 -0500

    Delete a meta tag of Astro

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width" />
-        <meta name="generator" content={Astro.generator} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
