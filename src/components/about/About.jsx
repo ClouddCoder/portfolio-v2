080315f82f2936c4f96f8c5809df1cde5adfc1b3
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-08-06 13:33:17 -0500

    Update the About main text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,21 +7,13 @@ function About() {
         <h2>About me</h2>
         <div className={styles["about-me"]}>
           <p>
-            I'm a <strong>Self-taught</strong> programmer and Systems engineering student from
-            Colombia with 1 year of experience as a software developer. My interest in technology
-            started when I got my first computer in 2009 and I decided to learn about ethical
-            hacking, that's how I acquired my
-            <strong>Linux</strong> and <strong>virtual machine</strong> skills.
+            <strong>Self-taught</strong> programmer and Systems engineering student from Colombia
+            with 1 year of experience as a software developer. Technology have always been my
+            passion and it allows me to reach many goals through my life.
           </p>
           <p>
-            Years later I entered the world of programming where I learned new techologies to build
-            web applications more focused on the <strong>front-end</strong> with the best practices.
-            As a result, I've developed efficient and scalable products and have become a
-            well-organized person, problem solver and team player.
-          </p>
-          <p>
-            My main focus these days is to deepen my <strong>back-end</strong> and{" "}
-            <strong>GCP</strong> skills to create products for the next generations.
+            Learning about Full stack skills is my main objective to scale in the technological
+            industry.
           </p>
         </div>
       </div>
