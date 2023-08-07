8c99a9bb878fae511bbea3f619451b44d3051c35
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-08-07 18:19:47 -0500

    Delete the head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
