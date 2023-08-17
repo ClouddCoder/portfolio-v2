fc4869e78b8556cc118153f9f51e3b48daa747b1
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-08-16 20:52:46 -0500

    Fix Thanks page css style

--- src/app/thanks/page.js
+++ src/app/thanks/page.js
@@ -3,16 +3,18 @@ import styles from "./page.module.css";
 
 function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.h1}>Thanks!</h1>
-        <p className={styles.p}>The form was submitted successfully.</p>
-        <p>You may return to the homepage.</p>
-        <Link className={styles.a} href="/">
-          Go to homepage
-        </Link>
-      </div>
-    </main>
+    <div className={styles["main-container"]}>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.h1}>Thanks!</h1>
+          <p className={styles.p}>The form was submitted successfully.</p>
+          <p>You may return to the homepage.</p>
+          <Link className={styles.a} href="/">
+            Go to homepage
+          </Link>
+        </div>
+      </main>
+    </div>
   );
 }
 
